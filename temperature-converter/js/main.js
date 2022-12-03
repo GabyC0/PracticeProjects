1ebd8aa22c786aecbf8e0a1a6caecde6ab4cf032
Gabriela Campos <74997368+GabyC0@users.noreply.github.com>
2022-12-02 21:40:55 -0800

    Update main.js

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -9,16 +9,19 @@ function convert() {
   //const convertToFahrenheit = (inputValue * 1.8) + 32;
 //display the value
   //console.log(convertedValue)
-    document.querySelector('#header').innerText = convertToCelsius;
+    //document.querySelector('#header').innerText = convertToCelsius;
     //document.querySelector('#header').innerText = convertToFahrenheit;
   
   //if (document.querySelector('input[name="tempSelection"]:checked').value == 1){
     if (convertToCelsius >= 29){
       document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
+      document.querySelector('#header').innerText = convertToCelsius;
     } else if (convertToCelsius < 29 && convertToCelsius >18){
-      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')"
+      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')";
+      document.querySelector('#header').innerText = convertToCelsius;
     } else {
-      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')"
+      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')";
+      document.querySelector('#header').innerText = convertToCelsius;
     } 
   }
 

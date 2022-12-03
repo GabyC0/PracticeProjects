f62b6f8828b11fe823a93478315322555228d899
Gabriela Campos <74997368+GabyC0@users.noreply.github.com>
2022-12-02 19:55:10 -0800

    Changed variable name to convetToCelsius

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -8,11 +8,11 @@ function convert() {
   const convertToFahrenheit = (inputValue * 1.8) + 32;
 //display the value
   //console.log(convertedValue)
-  document.querySelector('#header').innerText = convertedValue;
+    document.querySelector('#header').innerText = cconvertToCelsius;
 
-if (convertedValue >= 29){
+if (convertToCelsius >= 29){
   document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
-  } else if (convertedValue < 29 && convertedValue >18){
+  } else if (convertToCelsius < 29 && convertToCelsius >18){
     document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')"
   } else {
     document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')"

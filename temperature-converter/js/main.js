4fcbd0b4f24d5cb15dd20699e33b39967fd1d7d4
Gabriela Campos <74997368+GabyC0@users.noreply.github.com>
2022-12-02 20:35:45 -0800

    Added if statement for fahrenheit conversion

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -3,7 +3,7 @@ document.querySelector('#inputButton').addEventListener('click', convert);
 function convert() {
   //get value from an input
   const inputValue = document.querySelector('#input').value;
-  const radioSelection = document.querySelector('#').value;
+  const radioSelection = document.querySelector('#formInput').value;
   //the math here
   const convertToCelsius = (inputValue - 32) * 5/9;
   const convertToFahrenheit = (inputValue * 1.8) + 32;
@@ -11,13 +11,23 @@ function convert() {
   //console.log(convertedValue)
     document.querySelector('#header').innerText = convertToCelsius;
     document.querySelector('#header').innerText = convertToFahrenheit;
-
-if (convertToCelsius >= 29){
-  document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
-  } else if (convertToCelsius < 29 && convertToCelsius >18){
-    document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')"
-  } else {
-    document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')"
+  
+  if (radioSelection == "Celsius"){
+    if (convertToCelsius >= 29){
+      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
+    } else if (convertToCelsius < 29 && convertToCelsius >18){
+      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')"
+    } else {
+      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')"
+    } 
+  } else if (radioSelection == "Fahrenheit"){
+    if (convertToFahrenheit >= 84){
+      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
+    } else if (convertToCelsius < 84 && convertToCelsius >64){
+      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')"
+    } else {
+      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')"
+    } 
   }
 
 }

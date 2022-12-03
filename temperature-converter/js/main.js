c78c96453385d528bdb4f427213c7f93c9310879
Gabriela Campos <74997368+GabyC0@users.noreply.github.com>
2022-12-02 19:45:24 -0800

    Added fahrenheit equation

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -1,22 +1,21 @@
 //click event
-document.querySelector('#inputButton').addEventListener('click', convert)
+document.querySelector('#inputButton').addEventListener('click', convert);
 function convert() {
   //get value from an input
-  const cel = document.querySelector('#input').value
+  const inputValue = document.querySelector('#input').value;
   //the math here
-  const convertedValue = (cel - 32) * 5/9
+  const convertToCelsius = (inputValue - 32) * 5/9;
+  const convertToFahrenheit = (inputValue * 1.8) + 32;
 //display the value
   //console.log(convertedValue)
-  document.querySelector('#header').innerText = convertedValue
+  document.querySelector('#header').innerText = convertedValue;
 
 if (convertedValue >= 29){
-  document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')"
-}else if (
-convertedValue < 29 && convertedValue >18){
-
-document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')"
-}else {
-  document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')"
-}
+  document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
+  } else if (convertedValue < 29 && convertedValue >18){
+    document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')"
+  } else {
+    document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')"
+  }
 
 }

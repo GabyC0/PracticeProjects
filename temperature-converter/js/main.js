c94ce1414bf0c694f680d93eb74ed1877aea00e7
Gabriela Campos <74997368+GabyC0@users.noreply.github.com>
2022-12-11 13:25:52 -0800

    Testing ranges

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -8,26 +8,32 @@ function convert() {
   const optionVal = Number(document.querySelector('#temperature').value);
   //the math to convert & display amount converted to
   const convertToCelsius = (inputValue - 32) * 5/9;
-  const celsius = document.querySelector('#header').innerText = convertToCelsius;
+  //const celsius = document.querySelector('#header').innerText = convertToCelsius;
   const convertToFahrenheit = (inputValue * 1.8) + 32;
-  const fahrenheit = document.querySelector('#header').innerText = convertToFahrenheit;
+  //const fahrenheit = document.querySelector('#header').innerText = convertToFahrenheit;
   //background images
-  const image1 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
-  const image2 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')";
-  const image3 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')";
+  //const image1 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
+  //const image2 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')";
+  //const image3 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')";
   //if statement to account for option & temp range -> render background img and conversion
   if(optionVal === 1 && convertToCelsius >= 29){
-    celsius , image1;
+    document.querySelector('#header').innerText = convertToCelsius;
+    document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
   } else if(optionVal === 1 && (convertToCelsius < 29 && convertToCelsius >= 18)) {
-    celsius, image2;
+    document.querySelector('#header').innerText = convertToCelsius;
+    document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')";
   } else if (optionVal === 1 && convertToCelsius < 18) {
-    celsius, image3;
+    document.querySelector('#header').innerText = convertToCelsius;
+    document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')";
   }else if (optionVal === 2 && convertToFahrenheit >= 84) {
-    fahrenheit, image1;
+    document.querySelector('#header').innerText = convertToFahrenheit;
+    document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
   } else if (optionVal === 2 && (convertToFahrenheit < 84 && convertToFahrenheit >= 64)) {
-    fahrenheit, image2;
+    document.querySelector('#header').innerText = convertToFahrenheit;
+    document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')";
   } else if (optionVal === 2 && convertToFahrenheit < 64) {
-    fahrenheit, image3;
+    document.querySelector('#header').innerText = convertToFahrenheit;
+    document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')";
   }
 }
 

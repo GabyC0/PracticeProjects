1f382988ef852dbb3a79d8a7635277b10cce4075
Gabriela Campos <74997368+GabyC0@users.noreply.github.com>
2022-12-11 13:30:09 -0800

    Update main.js

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -10,12 +10,7 @@ function convert() {
   const convertToCelsius = (inputValue - 32) * 5/9;
   //const celsius = document.querySelector('#header').innerText = convertToCelsius;
   const convertToFahrenheit = (inputValue * 1.8) + 32;
-  //const fahrenheit = document.querySelector('#header').innerText = convertToFahrenheit;
-  //background images
-  //const image1 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
-  //const image2 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')";
-  //const image3 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')";
-  //if statement to account for option & temp range -> render background img and conversion
+  //temp ranges to change background and display temp after being converted
   if(optionVal === 1 && convertToCelsius >= 29){
     document.querySelector('#header').innerText = convertToCelsius;
     document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";

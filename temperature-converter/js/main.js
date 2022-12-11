6147c8404d4322527f3ddc437373e22331cbae1f
Gabriela Campos <74997368+GabyC0@users.noreply.github.com>
2022-12-10 23:08:08 -0800

    added if statement

--- temperature-converter/js/main.js
+++ temperature-converter/js/main.js
@@ -5,38 +5,27 @@ function convert() {
   //get value from an input
   const inputValue = document.querySelector('#input').value;
   //get the value from dropdown options
-  const optionVal = Number(document.querySelector('#temperature').val;
-  //the math to convert
+  const optionVal = Number(document.querySelector('#temperature').value);
+  //the math to convert & display amount converted to
   const convertToCelsius = (inputValue - 32) * 5/9;
+  const celsius = document.querySelector('#header').innerText = convertToCelsius;
   const convertToFahrenheit = (inputValue * 1.8) + 32;
+  const fahrenheit = document.querySelector('#header').innerText = convertToFahrenheit;
   //background images
   const image1 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
   const image2 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')";
   const image3 = document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')";
-    //document.querySelector('#header').innerText = convertToCelsius;
-    //document.querySelector('#header').innerText = convertToFahrenheit;
-  
-  //if (document.querySelector('input[name="tempSelection"]:checked').value == 1){
-    if (convertToCelsius >= 29){
-      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
-      document.querySelector('#header').innerText = convertToCelsius;
-    } else if (convertToCelsius < 29 && convertToCelsius >18){
-      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')";
-      document.querySelector('#header').innerText = convertToCelsius;
-    } else {
-      document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')";
-      document.querySelector('#header').innerText = convertToCelsius;
-    } 
-  }
-
-//else if (document.querySelector('input[name="tempSelection"]:checked').value == 2){
-//     if (convertToFahrenheit >= 84){
-//       document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/e5kbmb3wX3J1S/giphy.gif')";
-//     } else if (convertToCelsius < 84 && convertToCelsius >64){
-//       document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/xT8qBqfCcrD50Ffq7e/giphy.gif')"
-//     } else {
-//       document.querySelector('html').style.backgroundImage = "url('https://media.giphy.com/media/KFUx0Rtz7p0HTzbJ7x/giphy.gif')"
-//     } 
-//}
-
-//}
+  //if statement to account for option & temp range -> render background img and conversion
+  if(optionVal === 1 && convertedToCelsius >= 29){
+    celsius , image1;
+  } else if(optionVal === 1 && (convertedToCelsius < 29 && convertedToCelsius >= 18)) {
+    celsius, image2;
+  } else if (optionVal === 1 && convertedToCelsius < 18) {
+    celsius, image3;
+  }else if (optionVal === 2 && convertedToFahrenheit >= 84) {
+    fahrenheit, image1;
+  } else if (optionVal === 2 && (convertedToFahrenheit < 84 && convertedToFahrenheit >= 64)) {
+    fahrenheit, image2;
+  } else if (optionVal === 2 && convertedToFahrenheit < 64) {
+    fahrenheit, image3;
+}
